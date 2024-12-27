170260c2c1aba802a35e60daf97ea6bb6a87e756
waanh <anhimovalera@gmail.com>
2024-12-27 21:19:15 +0300

    12

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
     testEnvironment: "jest-environment-jsdom",
+    transform: {
+      '^.+\\.js$': 'babel-jest', 
+      '^.+\\.css$': 'jest-transform-stub',  
+    },
+    moduleNameMapper: {
+      '\\.css$': 'identity-obj-proxy',  
+    },
+    transformIgnorePatterns: [
+      '/node_modules/(?!some-module-to-transform).+\\.js$',  
+    ],
   };
+  
   
